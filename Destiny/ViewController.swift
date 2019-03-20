66070fabcc7f8aa00a5b3d6ea5ca6a60fb07f850
Damian <damianjasinowski@icloud.com>
2019-03-20 19:32:08 +0000

    Initial Commit

---
+++ Destiny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Destiny
+//
+//  Created by Damian Jasinowski on 20/03/2019.
+//  Copyright © 2019 Damian Jasinowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
